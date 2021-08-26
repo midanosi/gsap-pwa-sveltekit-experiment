14443d6a83bf6b9c77a91ec9409a853738b4d582
Michael Hutchings <mdhutchings94@gmail.com>
2021-08-26 18:09:37 +0100

    add basic anim to sceneA

--- src/routes/sceneA.svelte
+++ src/routes/sceneA.svelte
@@ -3,86 +3,108 @@
 </script>
 
 <script lang="ts">
-	import Sun from '$lib/icons/Sun.svelte';
+	// import Sun from '$lib/icons/Sun.svelte';
 	import gsap from 'gsap';
 	import { onMount } from 'svelte';
+	// import { useMachine } from '@xstate/svelte';
+	// import { createMachine, assign } from 'xstate';
+	// import Step1 from '$lib/sceneA/Step1.svelte';
 
-	let visible = false;
-	const duration = 1;
+	const setCurrentLabel = () => {
+		currentLabel = tl.currentLabel();
+		// console.log(`currentLabel`, currentLabel);
+	};
 
-	function tweenIn(node, somethingelse) {
-		console.log(`somethingelse`, somethingelse);
-		let tl = gsap.timeline();
+	let currentLabel = '';
+	let tl = gsap.timeline({
+		onUpdate: setCurrentLabel,
+		// paused: true
+		delay: 0.5
+	});
 
-		tl.from(node, {
-			duration,
-			opacity: 0
-		})
+	onMount(() => {
+		tl.addLabel('start')
+			.from('#intro-text-layer', { autoAlpha: 0 })
+			.addLabel('intro')
+			.to('#intro-text-layer', { autoAlpha: 0 }, '+=1')
+			.from('#outside-house-layer', { autoAlpha: 0 })
+			.from('#house', { autoAlpha: 0 })
+			.from('#sun', { autoAlpha: 0 })
 			.from(
 				'#circle',
 				{
-					duration,
 					'stroke-dashoffset': 2 * Math.PI * 4
-					// 'stroke-dasharray': 2 * Math.PI * 4
 				},
-				`-=${duration * 0.75}`
+				'<'
 			)
-			.from(
-				'.icon',
-				{
-					duration,
-					rotation: -40,
-					ease: 'ease.in'
-				},
-				`-=${duration * 0.1}`
-			)
+			.from('#sun', {
+				rotation: -40,
+				ease: 'ease.in'
+			})
 			.from(
 				'#outer-dashes',
 				{
 					ease: 'ease.in',
 					opacity: 0
 				},
-				`-=${duration * 0.8}`
-			);
-
-		return {
-			/* GSAP's duration is in seconds. Svelte's is in miliseconds */
-			duration: duration * 1000,
-			tick: (t) => {
-				tl.progress(t);
-			}
-		};
-	}
+				'-=0.2'
+			)
+			.addLabel('house');
+		tl.play('intro');
+		// tl.play('house');
+	});
 </script>
 
-<section>
-	<h1>Scene A</h1>
-	<label>
-		visible
-		<input bind:checked={visible} type="checkbox" />
-	</label>
-	{#if visible}
-		<svgF
-			xmlns="http://www.w3.org/2000/svg"
-			class="icon icon-tabler icon-tabler-sun"
-			id="icon-tabler-sun"
-			viewBox="0 0 24 24"
-			stroke-width="2"
-			stroke="currentColor"
-			fill="none"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			width="4rem"
-			height="4rem"
-			in:tweenIn
-		>
-			<circle id="circle" cx="12" cy="12" r="4" stroke-dasharray={2 * Math.PI * 4} fill="gold" />
-			<path
-				id="outer-dashes"
-				d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7"
-			/>
-		</svgF>
-	{/if}
+<section id="gsap-debugger">
+	<span>gsap debugger</span>
+	<span>current label: {currentLabel}</span>
+</section>
+<section id="animation-surface">
+	<div id="intro-text-layer">
+		<div class="textbox">
+			<h3>Introduction to Scene A</h3>
+			<p>Common problem is being outside house and not knowing what's inside</p>
+		</div>
+	</div>
+	<div id="outside-house-layer">
+		<div id="svgs-container">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				id="house"
+				width="200px"
+				height="200px"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="black"
+				fill="brown"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<polyline points="5 12 3 12 12 3 21 12 19 12" />
+				<path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
+				<path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
+			</svg>
+			<svg
+				id="sun"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				stroke-width="2"
+				stroke="currentColor"
+				fill="none"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				width="4rem"
+				height="4rem"
+			>
+				<circle id="circle" cx="12" cy="12" r="4" stroke-dasharray={2 * Math.PI * 4} fill="gold" />
+				<path
+					id="outer-dashes"
+					d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7"
+				/>
+			</svg>
+		</div>
+	</div>
 </section>
 
 <svelte:head>
@@ -95,5 +117,42 @@
 		flex-direction: column;
 		align-items: center;
 		flex: 1;
+	}
+	#animation-surface {
+		position: relative;
+		width: 100%;
+	}
+	#animation-surface > div {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	#gsap-debugger {
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
+
+	#svgs-container {
+		position: relative;
+		width: 600px;
+		height: 400px;
+	}
+	#house {
+		position: absolute;
+		bottom: 0;
+		left: 50%;
+		transform: translateX(-50%);
+	}
+	#sun {
+		position: absolute;
+		top: 0;
+		right: 20%;
 	}
 </style>
